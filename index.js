4fd277cce110a008b27a025dee0d2c3c3addec4b
Usman Muhammad Hassan <hassanusman5667@gmail.com>
2022-09-21 23:47:08 +0100

    created a basic server and updated a route using the get method

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express');
 // storing the express function in a variable
 const app = express()
 
+// using a get request to specify routes
+app.get('/', (req,res) => {
+    res.send('<h1>Hello World</h1>')
+})
+
 // configuring our port
 const PORT = process.env.PORT || 5000
 
